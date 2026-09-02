ed3f8ca557f823767b82dcf8cd6c6f140afe6b67
Kaarthikarameshkumar <192412294.simats@saveetha.com>
2026-09-02 09:21:07 +0530

    Add files via upload

---
+++ LAB/Prolog/fruit color.pl
@@ -0,0 +1,9 @@
+% Fruit and Color using Backtracking
+
+fruit_color(apple, red).
+fruit_color(banana, yellow).
+fruit_color(mango, yellow).
+fruit_color(orange, orange).
+fruit_color(grapes, green).
+fruit_color(watermelon, green).
+fruit_color(strawberry, red).
